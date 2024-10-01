5f9fb3b6cae99a800c67b783ec8204d81106c1e3
Evinurrahmawati <evinurrahmawati66@gmail.com>
2024-10-01 13:54:11 +0700

    tugas5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,6 +11,24 @@ use App\Http\Requests\UserRequest;
 
 class UserController extends Controller
 {
+        public $userModel;
+        public $kelasModel;
+        public function __construct()
+    {
+    $this->userModel = new UserModel();
+    $this->kelasModel = new Kelas();
+    }
+
+    public function index() 
+        { 
+            $data = [ 
+                'title' => 'Create User', 
+                'kelas' => $this->userModel->getUser(), 
+            ]; 
+        
+            return view('list_user', $data); 
+        }
+
     public function profile($nama = "", $kelas = "", $npm = "") {
         $data = [
             'nama' => $nama,
@@ -20,31 +38,33 @@ class UserController extends Controller
         return view ('profile', $data);
     }
 
-    public function create() {
-        return view ('create_user', [
-            'kelas' => Kelas::all(),
-        ]);
+    // public function create() {
+    //     return view ('create_user', [
+    //         'kelas' => Kelas::all(),
+    //     ]);
+    // }
+
+    public function create(){
+        $kelasModel = new Kelas ();
+
+        $kelas = $this->kelasModel->getKelas();
+
+        $data = [
+            'title' => 'Create User',
+            'kelas' => $kelas,
+        ];
+
+        return view('create_user', $data);
     }
 
-    public function store(UserRequest $request) {
-        $validatedData = $request->validate([
-            'nama' => 'required|string|max:255',
-            'npm' => 'required|string|max:255',
-            'kelas_id' => 'required|exists:kelas,id',
-        ]);
-
-        $user = UserModel::create($validatedData);
-        $user->load('kelas');
-
-
-        return view('profile', [
-            'nama' => $user->nama,
-            'npm' => $user->npm,
-            'nama_kelas' => $user->kelas->nama_kelas ?? 'Kelas tidak ditemukan',
-           
-        ]);
+    public function store(Request $request) 
+    { 
+        $this->userModel->create([ 
+        'nama' => $request->input('nama'), 
+        'npm' => $request->input('npm'), 
+        'kelas_id' => $request->input('kelas_id'), 
+        ]); 
         
-
-        
-    }
+        return redirect()->to('/user'); 
+    } 
 }
